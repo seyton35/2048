5ce69aeeacc113437bac1121e392c6901778989e
seyton35 <prosto99-00@mail.ru>
2023-04-03 05:06:12 +0300

    .

--- src/components/native/Cell.tsx
+++ src/components/native/Cell.tsx
@@ -9,44 +9,102 @@ interface Props {
     y: number,
 }
 
-export default function Cell({ size, count, x, y, }: Props) {
+const WEIGHT_COLORS: { [key: number]: string } = {
+    2: '#f1e9e2',
+    4: '#ebdfc8',
+    8: '#f1b177',
+    16: '#f49663',
+    32: '#f57a5f',
+    64: '#f45f3a',
+    128: '#ebcf72',
+    256: '#ebcd61',
+    512: '#ebc84e',
+    1024: '#ebc63e',
+    2048: '#ebc12d',
+}
+
+export default function Cell({ size, count, x, y }: Props) {
     const posX = useRef(new Animated.Value(x * size)).current
     const posY = useRef(new Animated.Value(y * size)).current
+    const scale = useRef(new Animated.Value(1)).current
 
-    const [counter, setcounter] = useState(count)
+    const [counter, setCounter] = useState(count)
 
     useEffect(() => {
-        move(posX, x * size)
+        bulgingCell()
+        setCounter(count)
+    }, [count])
+
+    useEffect(() => {
+        riseCell()
+    }, [])
+    useEffect(() => {
+        move(posX, x)
     }, [x])
     useEffect(() => {
-        move(posY, y * size)
+        move(posY, y)
     }, [y])
 
 
-    function move(direction: Animated.Value, newPos: number) {
-        Animated.spring(
-            direction,
-            {
-                toValue: newPos,
-                useNativeDriver: true
-            }
-        ).start(({ finished }) => {
-            if (finished) {
-                // doubleCounter()
-            }
-        })
+    function bulgingCell() {
+        Animated.sequence([
+            Animated.timing(
+                scale,
+                {
+                    toValue: 1.2,
+                    useNativeDriver: true,
+                    duration: 100
+                }),
+            Animated.timing(
+                scale,
+                {
+                    toValue: 1,
+                    useNativeDriver: true,
+                    duration: 100
+                })
+        ]).start()
     }
 
-    function moveTo() {
+    function riseCell() {
+        Animated.sequence([
+            Animated.timing(
+                scale,
+                {
+                    toValue: 0,
+                    useNativeDriver: true,
+                    duration: 0
+                }),
+            Animated.timing(
+                scale,
+                {
+                    toValue: 1,
+                    useNativeDriver: true,
+                    duration: 100
+                })
+        ]).start()
+    }
+
+    function move(direction: Animated.Value, newPos: number) {
+        Animated.timing(
+            direction,
+            {
+                toValue: newPos * size,
+                useNativeDriver: true,
+                duration: 500
+            }).start()
+    }
+
+    function animation() {
         return {
             transform: [
                 { translateX: posX },
-                { translateY: posY }
+                { translateY: posY },
+                { scale }
             ]
         }
     }
 
-    function getCellStyle() {
+    function getSize() {
         const style = {
             width: size,
             height: size,
@@ -54,18 +112,20 @@ export default function Cell({ size, count, x, y, }: Props) {
         return style
     }
     function getCellBackgroundStyle() {
+        const color = WEIGHT_COLORS[count]
         const style = {
             width: size - 4,
             height: size - 4,
+            backgroundColor: color
         }
         return style
     }
 
     return (
-        <Animated.View style={[styles.cell, getCellStyle(), moveTo()]}>
+        <Animated.View style={[styles.cell, getSize(), animation(),]}>
             <View style={[styles.cellBackground, getCellBackgroundStyle()]}>
-
                 <Txt style={styles.cellTxt}>{counter}</Txt>
+                {/* <Txt style={styles.cellTxt}>{`x:${x} y:${y}`}</Txt> */}
             </View>
         </Animated.View>
     )
@@ -80,7 +140,8 @@ const styles = StyleSheet.create({
     cellBackground: {
         alignItems: 'center',
         justifyContent: 'center',
-        backgroundColor: '#80ff80',
+        // backgroundColor: '#80ff80',
+        borderRadius: 10
     },
     cellTxt: {
         fontWeight: '900'
